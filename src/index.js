db3bb7d721cf2037a75fde3e31cbabf1b5971d51
Nicholas Dobie <nick@dobie.me>
2017-09-03 11:39:02 -0500

    feat: Created export index file
    This file will handle building and exposing parts of reserializer for use in other projects.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Utilities
+export {default as at} from './at'
+export {default as create} from './create'
+
+// Transforms
+export {default as asJSON} from './transforms/asJSON'
+export {default as asPrettyJSON} from './transforms/asPrettyJSON'
+export {default as date} from './transforms/date'
+export {default as remove} from './transforms/remove'
+export {default as rename} from './transforms/rename'
