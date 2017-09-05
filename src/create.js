15d1e2985de31f56522d382e204d6260e06cd194
Nicholas Dobie <nick@dobie.me>
2017-09-05 11:28:09 -0500

    feat: Created the create method
    This is used to build the final serializer.

---
+++ src/create.js
@@ -0,0 +1,52 @@
+export const CREATE_ID = Symbol('CREATE_ID') 
+
+export default function create(...transforms) {
+  return {
+    serialize (input) {
+      const runner = (rawItem) => {
+        // Make copy of transforms
+        const tempTransforms = transforms.slice()
+  
+        let serializedItem = rawItem
+        let transform
+  
+        while(transform = tempTransforms.pop()) {
+          serializedItem = transform.serialize(serializedItem)
+        }
+
+        return serializedItem
+      }
+
+      if (Array.isArray(input)) {
+        return input.map(item => runner(item))
+      } else {
+        return runner(input)
+      }
+    },
+
+    unserialize (input) {
+      const runner = (rawItem) => {
+        // Make copy of transforms
+        const tempTransforms = transforms.slice()
+        tempTransforms.reverse()
+  
+        let serializedItem = rawItem
+        let transform
+  
+        while(transform = tempTransforms.pop()) {
+          serializedItem = transform.unserialize(serializedItem)
+        }
+
+        return serializedItem
+      }
+
+      if (Array.isArray(input)) {
+        return input.map(item => runner(item))
+      } else {
+        return runner(input)
+      }
+    },
+
+    [CREATE_ID]: true
+  }
+}
