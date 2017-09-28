6919a62dbd0e856dd7b8d9913577f73a4250f82b
Nicholas Dobie <nrdobie@users.noreply.github.com>
2017-09-28 17:14:05 -0500

    feat: Added restructure transform (#17)
    * feat: Added flatten utility
    
    * feat: Added restructure
    
    * style: Lint fixes
    
    * test: Added test for key path support
    
    * docs: Documented resturcture
    
    * feat: Included restructure for release

---
+++ src/utils/flatten.js
@@ -0,0 +1,30 @@
+import isPlainObject from 'lodash.isplainobject'
+
+export default function flatten (map, prefix) {
+  return Object
+    .keys(map)
+    .filter(key => map.hasOwnProperty(key))
+    .reduce((flatMap, key) => {
+      const inputKey = prefix ? `${prefix}.${key}` : key
+      const outputKey = map[key]
+
+      if (isPlainObject(outputKey)) {
+        const nestedMap = flatten(outputKey, inputKey)
+
+        return {
+          input: Object.assign({}, flatMap.input, nestedMap.input),
+          output: Object.assign({}, flatMap.output, nestedMap.output)
+        }
+      }
+
+      if (outputKey === true) {
+        flatMap.input[inputKey] = inputKey
+        flatMap.output[inputKey] = inputKey
+      } else {
+        flatMap.input[inputKey] = outputKey
+        flatMap.output[outputKey] = inputKey
+      }
+
+      return flatMap
+    }, {input: {}, output: {}})
+}
---
+++ src/utils/flatten.test.js
@@ -0,0 +1,117 @@
+import flatten from './flatten'
+
+describe('flatten', () => {
+  it('should return an object with input and output keys', () => {
+    const flattenedMap = flatten({})
+
+    expect(flattenedMap).toHaveProperty('input')
+    expect(flattenedMap).toHaveProperty('output')
+  })
+
+  it('should map the input and output on a single property', () => {
+    const map = {
+      test: 'newTest'
+    }
+
+    const expectedFlattenedMap = {
+      input: {
+        test: 'newTest'
+      },
+      output: {
+        newTest: 'test'
+      }
+    }
+
+    expect(flatten(map)).toEqual(expectedFlattenedMap)
+  })
+
+  it('should map the input and output on multiple properties', () => {
+    const map = {
+      test: 'newTest',
+      anotherProp: 'newAnotherProp'
+    }
+
+    const expectedFlattenedMap = {
+      input: {
+        test: 'newTest',
+        anotherProp: 'newAnotherProp'
+      },
+      output: {
+        newTest: 'test',
+        newAnotherProp: 'anotherProp'
+      }
+    }
+
+    expect(flatten(map)).toEqual(expectedFlattenedMap)
+  })
+
+  it('should handle same name on a property', () => {
+    const map = {
+      test: true
+    }
+
+    const expectedFlattenedMap = {
+      input: {
+        test: 'test'
+      },
+      output: {
+        test: 'test'
+      }
+    }
+
+    expect(flatten(map)).toEqual(expectedFlattenedMap)
+  })
+
+  it('should map the input and output on a nested input property', () => {
+    const map = {
+      _meta: {
+        test: 'newTest'
+      }
+    }
+
+    const expectedFlattenedMap = {
+      input: {
+        '_meta.test': 'newTest'
+      },
+      output: {
+        newTest: '_meta.test'
+      }
+    }
+
+    expect(flatten(map)).toEqual(expectedFlattenedMap)
+  })
+
+  it('should map the input and output on a nested key input', () => {
+    const map = {
+      '_meta.test': 'newTest'
+    }
+
+    const expectedFlattenedMap = {
+      input: {
+        '_meta.test': 'newTest'
+      },
+      output: {
+        newTest: '_meta.test'
+      }
+    }
+
+    expect(flatten(map)).toEqual(expectedFlattenedMap)
+  })
+
+  it('should map the input and output on a nested output property', () => {
+    const map = {
+      test: '_meta.newTest'
+    }
+
+    const expectedFlattenedMap = {
+      input: {
+        test: '_meta.newTest'
+      },
+      output: {
+        '_meta.newTest': 'test'
+      }
+    }
+
+    expect(flatten(map)).toEqual(expectedFlattenedMap)
+  })
+})
